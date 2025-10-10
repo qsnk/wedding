7eaedccc87b5ac2d7882f3656b9ec69fd228e8cd
Mikhail <mikhailroot01@gmail.com>
2025-10-11 01:21:59 +0300

    Добавлен и обновлен текст

--- static/js/index.js
+++ static/js/index.js
@@ -3,7 +3,21 @@ document.addEventListener('DOMContentLoaded', () => {
     let currentDate = new Date();
     let targetDate = new Date('2026-09-09');
     let remainingDays = Math.round((targetDate.getTime() - currentDate.getTime()) / (1000 * 60 * 60 * 24));
-    timer.innerText = `Осталось ${remainingDays} дней`;
-
-    console.log('<h1>Hello</h1>');
+    let day = getDaysWord(remainingDays);
+    timer.innerHTML = `Увидимся через <b>${remainingDays}</b> ${day}`;
 })
+
+function getDaysWord(days) {
+  let n = Math.abs(days) % 100;
+  let n1 = n % 10;
+  if (n > 10 && n < 20) {
+      return 'дней';
+  }
+  if (n1 > 1 && n1 < 5) {
+      return 'дня';
+  }
+  if (n1 === 1) {
+      return 'день';
+  }
+  return 'дней';
+}
